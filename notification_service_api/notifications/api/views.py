90de7a6e9680d5930395165e28e75b0d31add8c3
Alexey Baikov <baikov52@gmail.com>
2022-05-30 23:43:37 +0300

    add Client api view

--- notification_service_api/notifications/api/views.py
+++ notification_service_api/notifications/api/views.py
@@ -1,11 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 
-from notification_service_api.notifications.models import Mailing
+from notification_service_api.notifications.models import Client, Mailing
 
-from .serializers import MailingSerializer
+from .serializers import ClientSerializer, MailingSerializer
 
 
 class MailingViewSet(ModelViewSet):
     # permission_classes = [AllowAny]
     serializer_class = MailingSerializer
     queryset = Mailing.objects.all()
+
+
+class ClientViewSet(ModelViewSet):
+    serializer_class = ClientSerializer
+    queryset = Client.objects.all()
