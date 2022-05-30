6cd103acf29e0912150b5e3de3d0b36570a28b22
Alexey Baikov <baikov52@gmail.com>
2022-05-30 22:46:21 +0300

    add Operator model

--- notification_service_api/notifications/models.py
+++ notification_service_api/notifications/models.py
@@ -11,3 +11,18 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Operator(models.Model):
+    code = models.CharField(verbose_name="Код оператора", max_length=50, unique=True)
+    name = models.CharField(
+        verbose_name="Название оператора", max_length=50, blank=True
+    )
+
+    class Meta:
+        verbose_name = "Мобильный оператор"
+        verbose_name_plural = "Мобильные операторы"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.name if self.name else self.code
