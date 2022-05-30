91daa02fd0466e545d17eb1600cddb89fd4557fa
Alexey Baikov <baikov52@gmail.com>
2022-05-30 23:32:07 +0300

    add Operator serializer

--- notification_service_api/notifications/api/serializers.py
+++ notification_service_api/notifications/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from notification_service_api.notifications.models import Tag
+from notification_service_api.notifications.models import Operator, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ["id", "name"]
+
+
+class OperatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Operator
+        fields = ["id", "code", "name"]
