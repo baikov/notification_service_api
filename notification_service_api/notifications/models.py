7d0d302901e7f268dbd9b3bce7d3bd9cd6c371b9
Alexey Baikov <baikov52@gmail.com>
2022-05-30 22:52:33 +0300

    add Mailing model

--- notification_service_api/notifications/models.py
+++ notification_service_api/notifications/models.py
@@ -26,3 +26,38 @@ class Operator(models.Model):
 
     def __str__(self):
         return self.name if self.name else self.code
+
+
+class Mailing(models.Model):
+    """
+    Сущность "рассылка"
+    """
+
+    title = models.CharField(
+        verbose_name="Название рассылки", max_length=250, blank=True
+    )
+    start_datetime = models.DateTimeField(verbose_name="Дата и время запуска рассылки")
+    end_datetime = models.DateTimeField(verbose_name="Дата и время окончания рассылки")
+    text = models.CharField(verbose_name="Текст рассылки", max_length=250)
+    tags = models.ManyToManyField(
+        Tag, verbose_name="Теги", related_name="mailings", blank=True
+    )
+    operator_code = models.ForeignKey(
+        Operator,
+        verbose_name="Код оператора",
+        help_text="Если оператор не выбран, то рассылка по всем",
+        related_name="mailings",
+        on_delete=models.PROTECT,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+        ordering = ("id",)
+
+    def __str__(self):
+        if self.title:
+            return f"{self.title} ({self.start_datetime}-{self.end_datetime})"
+        else:
+            return f"{self.mailing_text[:30]}... ({self.start_datetime}-{self.end_datetime})"
