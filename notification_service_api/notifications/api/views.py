cab666e2167aab431f61645ef5564d939c876e72
Alexey Baikov <baikov52@gmail.com>
2022-06-01 16:49:19 +0300

    add stats

--- notification_service_api/notifications/api/views.py
+++ notification_service_api/notifications/api/views.py
@@ -1,3 +1,8 @@
+from django.db.models import Count
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.serializers import ListSerializer
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from notification_service_api.notifications.models import Client, Mailing, Message
@@ -9,6 +14,28 @@ class MailingViewSet(ModelViewSet):
     # permission_classes = [AllowAny]
     serializer_class = MailingSerializer
     queryset = Mailing.objects.all()
+
+    @action(detail=True, url_path="stats")
+    def get_statistics(self, request, pk):
+        mailing = Mailing.objects.get(id=pk)
+        result = (
+            mailing.messages.all()
+            .values("status")
+            .annotate(count=Count("mailing"))
+            .order_by()
+        )
+        return Response(result, status=status.HTTP_200_OK)
+
+    @action(detail=True, url_path="msg")
+    def get_messages(self, request, pk):
+        mailing = Mailing.objects.get(id=pk)
+        messages = mailing.messages.all()
+        serializer = ListSerializer(
+            child=MessageSerializer(), context=self.get_serializer_context()
+        )
+        return Response(
+            serializer.to_representation(messages), status=status.HTTP_200_OK
+        )
 
 
 class ClientViewSet(ModelViewSet):
