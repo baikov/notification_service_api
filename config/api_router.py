dab70d43376800dd9a60ff4339f155f2a9dfd786
Alexey Baikov <baikov52@gmail.com>
2022-06-01 14:10:23 +0300

    fix routers

--- config/api_router.py
+++ config/api_router.py
@@ -1,22 +1,22 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-# from notification_service_api.users.api.views import UserViewSet
 from notification_service_api.notifications.api.views import (
     ClientViewSet,
     MailingViewSet,
     MessageViewSet,
 )
+from notification_service_api.users.api.views import UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-# router.register("users", UserViewSet)
 router.register("client", ClientViewSet)
 router.register("mailing", MailingViewSet)
 router.register("message", MessageViewSet)
+router.register("users", UserViewSet)
 
 
 app_name = "api"
